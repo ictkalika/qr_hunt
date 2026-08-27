af27856411eccb3babb0088d4d438f65bd539220
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-08-27 22:36:36 +0545

    fix: fixed the flaws in functions and made them working

--- utils/dataSearch.py
+++ utils/dataSearch.py
@@ -1,7 +1,13 @@
 import json
 import os
+from pathlib import Path
 
-DATA_FILE = "data.json"
+# So, here I am trying to get the root directory of this app
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+DATA_FILE = BASE_DIR / "data" / "data.json"
+ID_FILE = BASE_DIR / "data" / "id.json"
 
 
 def load_data():
@@ -14,11 +20,20 @@ def load_data():
         return {"error": "Invalid JSON format"}
 
 
-def search_data(id):
+def search_id(hex_code):
+    try:
+        with open(ID_FILE, "r", encoding="utf-8") as file:
+            id_map = json.load(file)
+        return id_map.get(hex_code)
+    except FileNotFoundError:
+        return {"error": "ID map file not found"}
+    except json.JSONDecodeError:
+        return {"error": "Invalid JSON format"}
+
+
+def search_data(item_id):
     data = load_data()
     if "error" in data:
         return data
-    for item in data:
-        if item["id"] == id:
-            return item
-    return {"error": "Item not found"}
+
+    return data.get(str(item_id), {"error": "Item not found"})
